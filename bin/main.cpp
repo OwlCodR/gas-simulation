6ed1afe5b3c1b3e7ce38171a09366dac297dfd6a
Maxim <maxonion@protonmail.com>
2021-02-20 15:38:54 +0300

    RGB Window

--- bin/main.cpp
+++ bin/main.cpp
@@ -5,27 +5,38 @@
 #include <vector>
 #include <string>
 #include <ctime>
+#include <math.h>
+
+// Only for Windows:
+#include <Windows.h>
 
 using namespace std;
 
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+void initialize() {
+	if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+}
+
 int main()
 {
-	time_t timer;
+	FreeConsole(); // Hide console
 
-	//Инициализация GLFW
-	glfwInit();
-	//Настройка GLFW
-	//Задается минимальная требуемая версия OpenGL. 
-	//Мажорная
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	//Минорная
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	//Установка профайла для которого создается контекст
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//Выключение возможности изменения размера окна
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwSetErrorCallback(error_callback);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Window", nullptr, nullptr);
+	initialize();
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Gas Simulation", nullptr, nullptr);
+
 	if (window == nullptr)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -33,12 +44,30 @@ int main()
 		return -1;
 	}
 
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
 	glfwMakeContextCurrent(window);
+
+	double time = glfwGetTime();
 	while(!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+		time = glfwGetTime();
+
+		/*
+		* Draw here!
+		*/
+
+		glClearColor(0.2f * sin(time), 0.7f * sin(time), 0.8f * cos(time), 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
