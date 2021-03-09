c0761fc616d1ff4dbf384635aa55c678dce62729
Maxim <maxonion@protonmail.com>
2021-03-09 20:19:35 +0300

    Added simple camera

--- bin/main.cpp
+++ bin/main.cpp
@@ -15,14 +15,40 @@
 #include "shader.cpp"
 #include "shader.h"
 
-// Only for Windows:
-#include <Windows.h>
-#include <filesystem>
-
 using namespace std;
+
+float speed = 0.1f;
+bool keys[1024];
 
 void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    	glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (action == GLFW_PRESS)
+		keys[key] = true;
+	else if (action == GLFW_RELEASE)
+		keys[key] = false;
+}
+
+void moveView(glm::mat4& view) {
+	glm::vec3 deltaVec(0.0f, 0.0f, 0.0f);
+
+	if (keys[GLFW_KEY_W])
+		deltaVec.z += speed;
+	else if (keys[GLFW_KEY_S])
+    	deltaVec.z -= speed;
+
+	if (keys[GLFW_KEY_A])
+    	deltaVec.x += speed;
+	else if (keys[GLFW_KEY_D])
+    	deltaVec.x -= speed;
+	
+	view = glm::translate(view, deltaVec);
 }
 
 void initialize() {
@@ -32,17 +58,18 @@ void initialize() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); 
 }
 
 int main()
 {
-	//FreeConsole(); // Hide console (Windows)
-	initialize();
-	glfwSetErrorCallback(error_callback);
-
 	double time = glfwGetTime();
+	initialize();
+
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Gas Simulation", nullptr, nullptr);
+	
+	glfwSetErrorCallback(error_callback);
+	glfwSetKeyCallback(window, key_callback); 
 
 	if (window == nullptr) {
 		cout << "Failed to create GLFW window" << endl;
@@ -64,9 +91,12 @@ int main()
 
 	Shader shader(vertexShaderPath, fragmentShaderPath);
 
-	glm::mat4 projection = glm::perspective(45.0f, (float) width / (float) height, 0.1f, 100.0f);
-	glm::mat4 model = glm::rotate(model, -55.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-	glm::mat4 view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	glm::mat4 projection(1.0f);
+	projection = glm::perspective(45.0f, (float) width / (float) height, 0.1f, 100.0f);
+	glm::mat4 model(1.0f);
+	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	glm::mat4 view(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, 1.0f));
 
 	GLfloat triangles[2][9] = {
 		{
@@ -78,7 +108,7 @@ int main()
 			-0.5f, 0.5f, 0.0f,
 			-0.5f, -0.5f, 0.0f,
 			-0.7f,  0.0f, 0.0f
-		}  
+		}
 	};
 
 	GLuint VBOs[2], VAOs[2];
@@ -95,7 +125,6 @@ int main()
 		glBindVertexArray(0);
 	}
 	
-	glm::mat4 trans(1.0f);
 	while(!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
@@ -103,10 +132,9 @@ int main()
 
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
-		//trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-		trans = glm::rotate(trans, (GLfloat) time, glm::vec3(0.0f, 0.0f, 1.0f));
 
+		moveView(view);
+		
 		shader.use();
 		
 		glUniformMatrix4fv(glGetUniformLocation(shader.program, "model"), 1, GL_FALSE, glm::value_ptr(model));
@@ -114,12 +142,10 @@ int main()
 		glUniformMatrix4fv(glGetUniformLocation(shader.program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
 		glUniform4f(glGetUniformLocation(shader.program, "currentColor"), sin(time), 0.0f, 1.0f, 1.0f);
-		glUniformMatrix4fv(glGetUniformLocation(shader.program, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
 		glBindVertexArray(VAOs[0]);
 			glDrawArrays(GL_TRIANGLES, 0, 6);
 		glBindVertexArray(0);
-		
-		glUniformMatrix4fv(glGetUniformLocation(shader.program, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+
 		glUniform4f(glGetUniformLocation(shader.program, "currentColor"), 0.0f, sin(time), 0.0f, 1.0f);
 		glBindVertexArray(VAOs[1]);
 			glDrawArrays(GL_TRIANGLES, 0, 6);
--- bin/shaders/shader.vert
+++ bin/shaders/shader.vert
@@ -5,9 +5,8 @@ layout (location = 0) in vec3 position;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
-uniform mat4 transform;
 
 void main()
 {
-    gl_Position = transform * vec4(position, 1.0f);
+    gl_Position = projection * view * model * vec4(position, 1.0f);
 }
